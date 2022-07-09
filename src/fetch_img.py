13efe95d2ffb61ba52951d33d78f4e99498ca328
Masato Tsuchiya <malk271828@gmail.com>
2022-07-09 21:58:54 +0900

    [update] output meta data

--- src/fetch_img.py
+++ src/fetch_img.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import os
+import json
 
 from rich import print
 from base64 import b64encode, b64decode
@@ -34,8 +35,8 @@ def gsv_metadata(lat, lon):
         print("WARN: No metadata for {0:f},{1:f}".format(lat, lon))
         return None
 
-def export_image_to_file(data, file_path):
-    url = '{0:s}'.format(file_path)
+def export_image_to_file(data, img_path):
+    url = '{0:s}'.format(img_path)
     with open(url, 'wb') as f:
         raw = b64decode(data)
         f.write(raw)
@@ -65,19 +66,31 @@ if __name__=="__main__":
     args = parseArgs()
     df = pd.read_csv(args.input)
     total = len(df)
+
+    # sampling
     df = df[:args.num]
 
-    # get metadata --------------
-    # df['gsv_metadata'] = df[['start_lat', 'start_long']].apply(lambda r: gsv_metadata(r.start_lat, r.start_long), axis=1)
-    # print(df["gsv_metadata"])
-
     for i, row in df.iterrows():
-        file_path = args.out + "img_lat{0}_lon{1}.jpg".format(row.start_lat, row.start_long)
-        if not os.path.exists(file_path):
-            img = gsv_image(row.start_lat, row.start_long, 0, 0)
-            export_image_to_file(img, file_path=file_path)
-            print("[cyan] save: {0} [/cyan]".format(file_path))
+        # get metadata ----------------------------------
+        meta_path = args.out + "meta/meta_lat{0}_lon{1}.json".format(row.start_lat, row.start_long)
+        if not os.path.exists(meta_path):
+            gsv_meta = gsv_metadata(row.start_lat, row.start_long)
+            meta = pd.concat([row, pd.Series(gsv_meta)], axis=0)
+            with open(meta_path, mode='w', encoding='utf-8') as fd:
+                json_str = meta.to_json(orient='index')
+                parsed = json.loads(json_str)
+                json.dump(parsed, fd, ensure_ascii=False, indent=4)
+            print("[cyan] save: {0} [/cyan]".format(meta_path))
         else:
-            print("[green] skipped: {0} [/green]".format(file_path))
+            print("[green] skipped: {0} [/green]".format(meta_path))
+
+        # get images by invoking Google Map APIs --------
+        img_path = args.out + "img/img_lat{0}_lon{1}.jpg".format(row.start_lat, row.start_long)
+        if not os.path.exists(img_path):
+            img = gsv_image(row.start_lat, row.start_long, 0, 0)
+            export_image_to_file(img, img_path=img_path)
+            print("[cyan] save: {0} [/cyan]".format(img_path))
+        else:
+            print("[green] skipped: {0} [/green]".format(img_path))
 
     print("{0}/{1} images".format(args.num, total))
